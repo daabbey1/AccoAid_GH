4062595fcd74856835c7894e45d5d14b33e9f0c6
daabbey1 <dicksonabbey4515@gmail.com>
2022-06-03 20:41:52 +0000

    FAQ

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -3,6 +3,7 @@
     <title> {{$name}}  </title>
     <div class="faq-body">
         <div class="faq">
+            {{-- Hero section --}}
             <div class="row">
                 <div class="col-6"> 
                     <div class="faq-title">
@@ -21,28 +22,178 @@
                     <img class="mission-image" src="images\faq.png" alt="hero">
                 </div>
             </div>
-            <div class="row">
+
+            {{-- Tenant Section --}}
+            <div class="row tenant">
                 <div class="col-2">
-                    <div class="faq-tenant-title"> Tenants </div>
+                    <div class="faq-tenant-head"> Tenants </div>
                 </div>
                 <div class="col-10">
-                    <div class="row faq-sheet">
-                        <div class="col-6">
-                            <div class="faq-tenant-title">
-                                Lorem ipsum dolor sit amet.
+                    <div class="faq-sheet">
+                        <div class="row faq-row">
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
                             </div>
-                            <div class="faq-tenant-body">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
                             </div>
                         </div>
-                        <div class="col-6">
-                            <div class="faq-tenant-title">
-                                Lorem ipsum dolor sit amet.
+
+                        <div class="row faq-row">
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
                             </div>
-                            <div class="faq-tenant-body">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
                             </div>
                         </div>
+
+                        <div class="row faq-row">
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row faq-row">
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+            {{-- Landlords Section --}}
+            <div class="row tenant">
+                <div class="col-2">
+                    <div class="faq-tenant-head"> Landlords </div>
+                </div>
+                <div class="col-10">
+                    <div class="faq-sheet">
+                        <div class="row faq-row">
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row faq-row">
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row faq-row">
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row faq-row">
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="faq-tenant-title">
+                                    Lorem ipsum dolor sit amet ?
+                                </div>
+                                <div class="faq-tenant-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sint id eaque molestiae enim corrupti.
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
